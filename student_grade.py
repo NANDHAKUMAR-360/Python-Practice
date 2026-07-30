d2dc1f2f94a2fcc2457e61db0d7b9a5f711ce417
NANDHAKUMAR-360 <nandhunote9@gmail.com>
2026-07-30 20:38:35 +0530

    student grade

--- student_grade.py
+++ student_grade.py
@@ -1,1 +1,11 @@
-a=
+a = int(input(" Enter your mark : "))
+if a >= 90:
+    print("Grade A")
+elif a >= 80:
+    print("Grade B")
+elif a >= 70:
+    print("Grade C")
+elif a >= 60:
+    print("Grade D ")   
+else:
+    print("ARREAR")
