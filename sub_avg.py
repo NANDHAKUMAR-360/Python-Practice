8f38fe7641df1c0f2fad65fc8b948d2ed914ba4f
NANDHAKUMAR-360 <nandhunote9@gmail.com>
2026-08-05 12:36:24 +0530

    factorial

---
+++ sub_avg.py
@@ -0,0 +1,11 @@
+a=int(input("Subject 1: "))
+b=int(input("Subject 2: "))
+c=int(input("Subject 3: "))
+d=int(input("Subject 4: "))
+e=int(input("Subject 5: "))   
+print((a+b+c+d+e)/5)
+
+if ((a+b+c+d+e)/5)<=35:
+    print("Additional classes are required")
+else:   
+    print("You good to go")
